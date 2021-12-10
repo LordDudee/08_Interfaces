47507676af3f32b8692d6e860e466eabfd111b40
Dude <michael_runschke@gmx.de>
2021-12-10 11:40:48 +0100

    Interfaces und abstrakte Klasse

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,7 +1,14 @@
 package com.cc.java;
-public class App {
 
-    // static Honeybee worker;
+// import com.cc.java.bees.Drone;
+// import com.cc.java.bees.HoneyBee;
+// import com.cc.java.bees.Queen;
+// import com.cc.java.bees.Worker;
+import com.cc.java.bees.*;  // Alles aus dem Ordner importieren
+import com.cc.java.birds.Bird;
+
+
+public class App {
 
     public static void main(String[] args) {
       
@@ -9,15 +16,21 @@ public class App {
     HoneyBee queen = new Queen();
     HoneyBee drone = new Drone();
 
+    Bird bird = new Bird();
+
+    ausgabe("----------- Bees -----------");
     ausgabe(worker.doYourJob());
     ausgabe(queen.doYourJob());
     ausgabe(drone.doYourJob());
-
-    ausgabe("------------------");
-
+    
+    ausgabe("----------- Bird(s) -----------");
+    ausgabe(bird.hasFeathers());
+    
+    ausgabe("----------- Bees & Bird(s) -----------");
     ausgabe(worker.fly());
     ausgabe(queen.fly());
     ausgabe(drone.fly());
+    ausgabe(bird.fly());
 
     }
 
